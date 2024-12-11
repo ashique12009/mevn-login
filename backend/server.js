2eac899b4f75851fd93dbf49c187a0c3689dd430
ashique <ashique12009@gmail.com>
2024-12-11 21:36:51 +0200

    db connection done

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv');
+// Load environment variables from .env file
+dotenv.config();
+const db = require('./config/db');
+
+// Create Express app
+const app = express();
+
+// DB Connection
+db.connect((err) => {
+    if (err) {
+        console.error('Database connection failed:', err);
+    }
+    else {
+        console.log('Database connection successful');
+    }
+});
+
+// Server setup
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
