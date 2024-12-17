fbbee31065a919937e6eee98d5eb84acfd89c759
ashique <ashique12009@gmail.com>
2024-12-17 23:25:12 +0200

    logout done for frontend

--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -2,14 +2,20 @@
     <div class="inner-pages">
         <nav>
             <router-link to="/dashboard">Dashboard</router-link>
-            <router-link to="/">Logout</router-link>
+            <router-link to="/" @click="logout">Logout</router-link>
         </nav>
         <h1>Dashboard</h1>
     </div>
 </template>
 
 <script setup>
+    import { useRouter } from 'vue-router';
+    const router = useRouter();
 
+    const logout = () => {
+        localStorage.removeItem("authToken");
+        router.push("/");
+    }
 </script>
 
 <style lang="scss" scoped>
